83841237fbf6043f271926056c69210d1f4848ef
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-01 11:39:23 +0200

    models class created for person.cs and made public

--- eHealthFolderDev/Models/Person.cs
+++ eHealthFolderDev/Models/Person.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace eHealthFolderDev.Models
 {
-    internal class Person
+    public class Person
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int  IDNumber { get; set; }
+        public string Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Allergies { get; set; }
+        public string email { get; set; }
+        public string Gender { get; set; }
     }
 }
