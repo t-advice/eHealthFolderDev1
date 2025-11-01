86935a48a5bcc5cf5d8be16cd86fef71136a3dad
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-01 11:49:44 +0200

    Visits.cs model class created

--- eHealthFolderDev/Models/Visits.cs
+++ eHealthFolderDev/Models/Visits.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace eHealthFolderDev.Models
 {
-    internal class Visits
+    public class Visits
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Doctor { get; set; }
+        public string Department { get; set; }
+        public string Diagnosis { get; set; }
+        public string Notes { get; set; }
+        public string Hospital { get; set; }
+
+
     }
 }
