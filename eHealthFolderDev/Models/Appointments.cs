abbe6354bf8754797f824638cf079308462efe00
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-01 11:44:40 +0200

    appointments model class created and made public

--- eHealthFolderDev/Models/Appointments.cs
+++ eHealthFolderDev/Models/Appointments.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace eHealthFolderDev.Models
 {
-    internal class Appointments
+    public class Appointments
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Location { get; set;}
+        public string Hospital { get; set; }
+        public string Reason { get; set; }
+        public DateTime Date { get; set; }
+        public string Time { get; set; }
+        public string Notes { get; set; }
+
+
+
     }
 }
