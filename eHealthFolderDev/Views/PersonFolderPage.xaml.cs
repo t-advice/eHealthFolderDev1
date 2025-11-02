b70c4a4f99132d6a9d48d27090d0bde78a017fdf
Tashwill Advice <tashwilladvice02@gmail.com>
2025-11-02 13:42:43 +0200

    logic wired up for personfolderpage . click eventhandlers added for visits and appointments .Also added tap gestures

--- eHealthFolderDev/Views/PersonFolderPage.xaml.cs
+++ eHealthFolderDev/Views/PersonFolderPage.xaml.cs
@@ -1,9 +1,71 @@
+using eHealthFolderDev.Models;
+using eHealthFolderDev.Services;
+using System.ComponentModel;
+
+
 namespace eHealthFolderDev.Views;
 
 public partial class PersonFolderPage : ContentPage
 {
-	public PersonFolderPage()
+	private readonly eHealthFolderDatabase _database;
+    public PersonFolderPage(eHealthFolderDatabase database)
 	{
 		InitializeComponent();
+		_database = database;
+
+		LoadVisits();
+		LoadAppointments();
 	}
+	private async void LoadVisits()
+	{
+		var appoinments = await _database.GetVisitsAsync();
+		AppointmentsCollectionView.ItemsSource = visits;
+	}
+    private async void LoadAppointments()
+    {
+        var appoinments = await _database.GetAppointmentsAsync();
+        AppointmentsCollectionView.ItemsSource = appointments;
+    }
+    private void VisitsButton_Clicked(object sender, EventArgs e)
+    {
+        VisitsCollectionView.IsVisible = true;
+        AppointmentsCollectionView.IsVisible = false;
+
+        VisitsButton.BackgroundColor = Color.FromArgb("#A5D6A7");
+        AppointmentsButton.BackgroundColor = Color.FromArgb("#E0F7FA");
+    }
+    private void AppointmentsButton_Clicked(object sender, EventArgs e)
+    {
+        VisitsCollectionView.IsVisible = false;
+        AppointmentsCollectionView.IsVisible = true;
+
+        VisitsButton.BackgroundColor = Color.FromArgb("#E0F7FA");
+        AppointmentsButton.BackgroundColor = Color.FromArgb("#FFF3E0");
+    }
+
+    private async void VisitItem_Tapped(object sender, EventArgs e)
+    {
+        var frame = sender as Frame;
+        if (frame?.BindingContext is Visits visit)
+        {
+            await DisplayAlert("Visit Details",
+                $"Doctor: {visit.Doctor}\nDiagnosis: {visit.Diagnosis}\nTreatment: {visit.Treatment}",
+                "OK");
+        }
+    }
+
+    private async void AppointmentItem_Tapped(object sender, EventArgs e)
+    {
+        var frame = sender as Frame;
+        if (frame?.BindingContext is Appointments appointment)
+        {
+            await DisplayAlert("Appointment Details",
+                $"Hospital: {appointment.Hospital}\nReason: {appointment.Reason}\nDate: {appointment.Date:dd/MM/yyyy}",
+                "OK");
+        }
+    }
+}
+
+
+
 }
